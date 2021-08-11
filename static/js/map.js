8956e5925719f54f41924d6daf8933dfab0f46d0
SeanGau <sean@b1t.tw>
2021-08-11 17:51:29 +0800

    update index layout

--- static/js/map.js
+++ static/js/map.js
@@ -14,7 +14,7 @@ let satellite = L.tileLayer(MymbUrl, { id: 'ckmemz8hn1m4r17t8hu2sd80i', attribut
     attribution: mbAttr
   });
 
-function pcc_icon_style(color) {
+function icon_style(color) {
   return `
 	color: black;
 	background-color: ${color};
@@ -55,10 +55,16 @@ L.control.locate({
 
 mymap.addControl(new L.Control.Gps({ position: 'topright' }));
 const search = new GeoSearch.GeoSearchControl({
+  marker: {
+    icon: new L.Icon.Default(),
+    draggable: true,
+  },
   style: 'bar',
-  searchLabel: '搜尋位置',
-  autoClose: true,
-  keepResult: true,
+  searchLabel: '搜尋地點',
+  showPopup: true,
+  popupFormat: ({ query, result }) => {
+    return "開始標記你發現的坪林妖怪!把雲霧移動到發現的地點增加妖怪資料。"
+  },
   provider: new GeoSearch.OpenStreetMapProvider({
     params: {
       countrycodes: 'TW'
@@ -67,19 +73,23 @@ const search = new GeoSearch.GeoSearchControl({
 });
 mymap.addControl(search);
 
-var popup = L.popup().setContent(`
+var popup = `
 <h1>醉猴</h1>
 <p>哇哈哈</p>
 <a href="/monster/1" class="btn btn-warning">詳細資訊</a>
-`);
+`;
+var customPopupOptions = {
+  'className': 'cloud-popup'
+}
 let test_marker = L.marker([24.937602, 121.712626], {
   icon: L.divIcon({
     className: "custom-marker",
     iconAnchor: [0, 0],
     labelAnchor: [0, 0],
     popupAnchor: [60, 8],
-    html: `<span style="${pcc_icon_style("rgb(252,112,5)")}" />`
+    html: `<span style="${icon_style("rgb(252,112,5)")}" />`
   }),
   closeButton: false,
 }).addTo(mymap);
-test_marker.bindPopup(popup).openPopup();
+
+test_marker.bindPopup(popup, customPopupOptions).openPopup();
