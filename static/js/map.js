a62b6846669b38b718db5741bc0759022b7a35d4
SeanGau <sean@b1t.tw>
2021-08-19 12:18:31 +0800

    update frontend layout

--- static/js/map.js
+++ static/js/map.js
@@ -1,21 +1,21 @@
 let colors = {
-  "wind": "#7CAD82",
-  "fire": "#EE7839",
-  "ground": "#DBB853",
-  "water": "#5B9E98"
+    "wind": "#7CAD82",
+    "fire": "#EE7839",
+    "ground": "#DBB853",
+    "water": "#5B9E98"
 }
 
 let mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://www.mapbox.com/">mapbox</a> ',
-  MymbUrl = 'https://api.mapbox.com/styles/v1/js00193/{id}/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoianMwMDE5MyIsImEiOiJjazN0dnN2aDkwNmwxM21vM2lvNDB4ZzJkIn0.48gtpsBsdD2vLWDVe1dOlQ';
+    MymbUrl = 'https://api.mapbox.com/styles/v1/js00193/{id}/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoianMwMDE5MyIsImEiOiJjazN0dnN2aDkwNmwxM21vM2lvNDB4ZzJkIn0.48gtpsBsdD2vLWDVe1dOlQ';
 let satellite = L.tileLayer(MymbUrl, { id: 'ckmemz8hn1m4r17t8hu2sd80i', attribution: mbAttr }),
-  streets = L.tileLayer(MymbUrl, {
-    maxNativeZoom: 18,
-    id: 'ckova8spg0as318mobuqnirjh',
-    attribution: mbAttr
-  });
+    streets = L.tileLayer(MymbUrl, {
+        maxNativeZoom: 18,
+        id: 'ckova8spg0as318mobuqnirjh',
+        attribution: mbAttr
+    });
 
 function icon_style(color) {
-  return `
+    return `
 	color: black;
 	background-color: ${color};
 	width: 18px;
@@ -29,52 +29,54 @@ function icon_style(color) {
 };
 
 let mymap = L.map('map', {
-  center: [24.937602, 121.712626],
-  zoom: 18,
-  maxZoom: 18,
-  minZoom: 10,
-  zoomDelta: 0.25,
-  zoomSnap: 0,
-  layers: [streets],
-  zoomControl: false
+    center: [24.937602, 121.712626],
+    zoom: 18,
+    maxZoom: 18,
+    minZoom: 10,
+    zoomDelta: 0.25,
+    zoomSnap: 0,
+    layers: [streets],
+    zoomControl: false
 });
 
 L.control.zoom({
-  position: 'topright'
+    position: 'topright'
 }).addTo(mymap);
 
 let baseMaps = {
-  "空照圖": satellite,
-  "街道圖": streets,
+    "空照圖": satellite,
+    "街道圖": streets,
 };
 
 L.control.layers(baseMaps, null, { position: 'bottomright' }).addTo(mymap);
 L.control.locate({
-  position: 'topright'
+    position: 'topright'
 }).addTo(mymap);
 
 function popupAddNew(latlng) {
-  let popLocation = latlng;
-  let popup = L.popup({'className': 'cloud-popup'})
-    .setLatLng(popLocation)
-    .setContent('開始標記你發現的坪林妖怪!<br>把雲霧移動到發現的地點增加妖怪資料。')
-    .openOn(mymap);
+    let popLocation = latlng;
+    let popup = L.popup({
+        'className': 'cloud-popup',
+    })
+        .setLatLng(popLocation)
+        .setContent('開始標記你發現的坪林妖怪!<br>把雲霧移動到發現的地點增加妖怪資料。')
+        .openOn(mymap);
 }
 
 mymap.addControl(new L.Control.Gps({ position: 'topright' }));
 const search = new GeoSearch.GeoSearchControl({
-  style: 'bar',
-  searchLabel: '搜尋地點',
-  autoClose: true,
-  provider: new GeoSearch.OpenStreetMapProvider({
-    params: {
-      countrycodes: 'TW'
-    }
-  }),
+    style: 'bar',
+    searchLabel: '搜尋地點',
+    autoClose: true,
+    provider: new GeoSearch.OpenStreetMapProvider({
+        params: {
+            countrycodes: 'TW'
+        }
+    }),
 });
 mymap.addControl(search);
 mymap.on('geosearch/showlocation', function (e) {
-  popupAddNew(e.marker.getLatLng());
+    popupAddNew(e.marker.getLatLng());
 });
 
 
@@ -84,21 +86,21 @@ var popup = `
 <a href="/monster/1" class="btn btn-warning">詳細資訊</a>
 `;
 var customPopupOptions = {
-  'className': 'cloud-popup'
+    'className': 'cloud-popup'
 }
 let test_marker = L.marker([24.937602, 121.712626], {
-  icon: L.divIcon({
-    className: "custom-marker",
-    iconAnchor: [0, 0],
-    labelAnchor: [0, 0],
-    popupAnchor: [60, 8],
-    html: `<span style="${icon_style(colors.wind)}" />`
-  }),
-  closeButton: false,
+    icon: L.divIcon({
+        className: "custom-marker",
+        iconAnchor: [0, 0],
+        labelAnchor: [0, 0],
+        popupAnchor: [60, 8],
+        html: `<span style="${icon_style(colors.wind)}" />`
+    }),
+    closeButton: false,
 }).addTo(mymap);
 
 test_marker.bindPopup(popup, customPopupOptions).openPopup();
 
 mymap.on('click', function (e) {
-  popupAddNew(e.latlng);
+    popupAddNew(e.latlng);
 });
